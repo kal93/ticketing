d69f9324d0935f094094e52096996f5bcb020e5b
kal93 <maaz2dt@gmail.com>
2021-11-26 13:14:50 +0530

    add jwt libs add secret as env variables on kubernetes add jwt to cookie

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { json } from 'body-parser';
 import 'express-async-errors';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 import chalk from 'chalk';
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/sign-in';
@@ -13,7 +14,17 @@ import { NotFoundError } from './errors/not-found-error';
 const log = console.log;
 
 const app = express();
+// Set  trust proxy because traffic is being proxied to this app thro' ingress-nginx load balancer.
+// Express is aware by default of proxy connections. So make it trusted one.
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    // disable encryption as we'll be using JWT inside the cookie
+    signed: false,
+    secure: false, // setting it to true only allows for https connections
+  })
+);
 
 app.use(signUpRouter);
 app.use(currentUserRouter);
@@ -28,8 +39,8 @@ app.use(errorHandler);
 
 const start = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/auth');
-    // await mongoose.connect('mongodb://auth-mongo-srv:27017/auth'); // check cluster ip service for mongodb for url
+    // await mongoose.connect('mongodb://127.0.0.1:27017/auth');
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth'); // check cluster ip service for mongodb for url
     log('Connected to Mongo DB');
   } catch (error) {
     console.error(error);
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -4,8 +4,11 @@ const log = console.log;
 const router = express.Router();
 
 router.get('/api/users/currentUser', (req, res) => {
+  const locale = req.headers['accept-language'];
+  const locales = locale?.split(';').join(',').split(',');
   res.status(200).send({
-    msg: 'Hi there',
+    msg: 'Hi there....',
+    locales,
   });
 });
 
